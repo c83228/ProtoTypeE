d84f7440e4581268584146fb5424e9158a4a5350
c83228 <c83228@yahoo.com.tw>
2016-12-14 22:39:43 +0800

    12/14

---
+++ src/SCENERE/SceneRenderEngine.java
@@ -0,0 +1,14 @@
+package SCENERE;
+
+/**
+ * Created by hsu on 2016/12/14.
+ */
+public class SceneRenderEngine {
+
+    void renderScene{
+        
+
+    }
+
+
+}
---
+++ src/SDM/SceneDataModule.java
@@ -0,0 +1,14 @@
+package SDM;
+
+/**
+ * Created by hsu on 2016/12/14.
+ */
+public class SceneDataModule {
+
+    void loadMap(String mapfile){
+
+
+    }
+
+
+}
